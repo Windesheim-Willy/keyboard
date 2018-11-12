80b267e0ea3e4f30c5a735b268ad8873adfa8dd0
Johan2424 <johan@klimopgroep.nl>
2018-11-12 21:27:23 +0100

    Vooruit

--- src/move.py
+++ src/move.py
@@ -2,37 +2,41 @@
 import rospy
 from geometry_msgs.msg import Twist
 
-
-def move():
-    # Starts a new node
-    rospy.init_node('move_willy', anonymous=True)
-    velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
-    vel_msg = Twist()
-    
+def questions():
     #Receiveing the user's input
     print("Laten we Willy een dansje doen!")
     speed = input("Snelheid:")
     distance = input("Afstand:")
     isForward = input("Vooruit?: ")#True or False
-    isTurnRight = input("Bocht naar rechts?")#True or False
-    isTurnLeft = input("Bocht naar links?")#True or False
+
+def move():
+    # Starts a new node
+    rospy.init_node('move_willy', anonymous=True)
+    velocity_publisher = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+    vel_msg = Twist()
+    
+    #Receiveing the user's input
+    #print("Laten we Willy een dansje doen!")
+    #speed = input("Snelheid:")
+    #distance = input("Afstand:")
+    #isForward = input("Vooruit?: ")#True or False
     
     #Checking if the movement is forward or backwards
     if(isForward):
-        vel_msg.angular.z = abs(speed)
+        vel_msg.linear.x = abs(speed)
     else:
-        vel_msg.angular.z = -abs(speed)
+        vel_msg.linear.x = -abs(speed)
     
     #if(isTurnLeft):
     #    vel_msg.linear.z = abs(speed)
     
     #Since we are moving just in x-axis
-    vel_msg.linear.x = 0
+    #vel_msg.linear.x = 0
     vel_msg.linear.y = 0
     vel_msg.linear.z = 0
     vel_msg.angular.x = 0
     vel_msg.angular.y = 0
-    #vel_msg.angular.z = 0
+    vel_msg.angular.z = 0
     
     while not rospy.is_shutdown():
 
@@ -49,7 +53,7 @@ def move():
             #Calculates distancePoseStamped
             current_distance= speed*(t1-t0)
         #After the loop, stops the robot
-        vel_msg.angular.z = 0
+        vel_msg.linear.x = 0
         #Force the robot to stop
         velocity_publisher.publish(vel_msg)
 
